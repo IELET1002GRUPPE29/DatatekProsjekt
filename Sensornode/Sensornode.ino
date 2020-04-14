1d15a1a991cb9bbd79ea2f69b015efe84225ecf5
Michael <Berg>
2020-04-14 22:02:47 +0200

    95%
    Mangler litt servo

--- Sensornode/Sensornode.ino
+++ Sensornode/Sensornode.ino
@@ -1,16 +1,21 @@
-#include <ESP32Servo.h>
+#include <Servo.h>
 
 
 Servo myservo;  // create servo object to control a servo
 int pos = 0;    // variable to store the servo position
-int servoPin = 15;
+int servoPin = 33;
 bool ledstate = 1;
 int ltid  = 0; //DDDDDDDDDD
 int terskel_teller = 0;
 int terskel_temp = 28;
-int terskel_gass = 1000;
+int terskel_gass = 100;
 int terskel_lux = 500;
+#include <Wire.h>
 
+#include <Adafruit_PWMServoDriver.h>
+Adafruit_PWMServoDriver board1 = Adafruit_PWMServoDriver(0x40);
+#define SERVOMIN  125 // this is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX  575 // this is the 'maximum' pulse length count (out of 4096)
 
 //******BUZZER*******
 #define LEDC_CHANNEL_0  0
@@ -49,7 +54,7 @@ int resolution = 8;
 WidgetTerminal terminal(V5);
 WidgetLED alarmled(V14);
 BlynkTimer timer;
-Adafruit_VL6180X vl = Adafruit_VL6180X();
+Adafruit_VL6180X vl = Adafruit_VL6180X(); //BRUKER 0x29 I2C adresse
 
 // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 // ===============================================================================================================
@@ -58,7 +63,7 @@ Adafruit_VL6180X vl = Adafruit_VL6180X();
 //          GLOBALE VARIABLER
 
 unsigned long alarmtid = 0;    //Definerer tid for millis funksjon
-int alarmlengde = 30; //30s alarm (Varer like lenge som lengde mellom max/min målinger, kan dermed gi kontinuerlig alarm)
+int alarmlengde = 30000; //30s alarm (Varer like lenge som lengde mellom max/min målinger, kan dermed gi kontinuerlig alarm)
 int alarmos = 0;
 unsigned long tid_nu = 0;
 unsigned long tid = 0;    //Definerer tid for millis funksjon
@@ -81,15 +86,19 @@ int readIndex = 0;                //Indeksen til nåværende avlesning
 float total = 0;                  //Total for å finne gjennomsnitt
 float average = 0;                //Gjennomsnittet
 bool en_boolsk_verdi_for_utregning = 0;
-const int gassPin = 33;
-const int tempPin = 32;
+
+const int gassPin = 32;
+const int tempPin = 35;
 const int ledPin = 23;
+
+
 float maxverdiTemp = 0;
 float maxverdiLux = 0;
 float maxverdiGass = 0;
 float minverdiTemp = 10000; //Setter høyt slik at vi får lavere..
 float minverdiLux = 10000;
 float minverdiGass = 10000;
+
 char auth[] = "thi6MWmSU17ZP4nTzTsTdojm2wV5hJ2x";
 char ssid[] = "PBM";
 char pass[] = "pbmeiendom";
@@ -103,14 +112,18 @@ char pass[] = "pbmeiendom";
 void setup() {
   ledcSetup(LEDC_CHANNEL_0, LEDC_BASE_FREQ, LEDC_TIMER_13_BIT);
   ledcAttachPin(LED_PIN, LEDC_CHANNEL_0);
-  Serial.begin(115200);
-  myservo.setPeriodHertz(50);    // standard 50 hz servo
-  myservo.attach(servoPin, 250, 2300); // attaches the servo on pin 15 to the servo object
+  Serial.begin(9600);
+  //myservo.setPeriodHertz(50);    // standard 50 hz servo
+  myservo.attach(servoPin);//, 250, 2300); // attaches the servo on pin 15 to the servo object
   //Fyller array med 0
   for (int thisReading = 0; thisReading < numReadings; thisReading++) {
     avlesningerTemp[thisReading] = 0;
 }
 
+  board1.begin();
+  board1.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
+
+  
   pinMode(tempPin, INPUT);
   pinMode(gassPin, INPUT);
   pinMode(ledPin, OUTPUT);
@@ -224,10 +237,10 @@ float gjennomsnittArray(float * array, int len) {
 
 void myTimerEvent()
 {
-  aRead = analogRead(gassPin);                             //Leser av analog spenningsverdi
+  aRead = analogRead(tempPin);                             //Leser av analog spenningsverdi
   R = aRead / (4095 - aRead) * R_0;                   //Regner ut termistorresistansen
   temp = - 273.15 + 1 / ((1 / T_0) + (1 / b) * log(R / R_0)); //Regner ut temperaturen i C
-  gass = analogRead(tempPin);
+  gass = analogRead(gassPin);
   lux = vl.readLux(VL6180X_ALS_GAIN_5);
   Blynk.virtualWrite(V0, temp);
   Blynk.virtualWrite(V1, gass);
@@ -290,9 +303,23 @@ void myTimerEvent()
     en_boolsk_verdi_for_utregning = 1;
     readIndex = 0;
   }
+
+
+
+
+
+
   // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
   // ===============================================================================================================
 }
+
+  int angleToPulse(int ang){
+   int pulse = map(ang,0, 180, SERVOMIN,SERVOMAX);// map angle of 0 to 180 to Servo min and Servo max 
+   Serial.print("Angle: ");Serial.print(ang);
+   Serial.print(" pulse: ");Serial.println(pulse);
+   return pulse;
+}
+
 // ===============================================================================================================
 //          SETUP
 // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
@@ -310,7 +337,7 @@ void ledcAnalogWrite(uint8_t channel, uint32_t value, uint32_t valueMax = 255) {
 };
 
 
-
+//int statssss = 0;
 void loop() {
   Blynk.run();
   timer.run();
@@ -319,30 +346,55 @@ void loop() {
   
   //myservo.write(180);
   if (alarmos == 1) {
+    
     //LEDFLASH
-    if (tid_nu > 1000 + ltid) {  //Dersom det har gått 500ms siden sist tid
+    
+    if (tid_nu > 1000 + ltid){// && tid_nu <2000 + ltid && statssss == 0) {  //Dersom det har gått 500ms siden sist tid
       ledstate = !ledstate;     //Skift status
-      Serial.println(90 * ledstate);
-      myservo.write(180 * ledstate);
+            Serial.println("ALARM 2");
+            
       //alarmled.setValue(ledstate*255);
       digitalWrite(ledPin, ledstate);   //Lys
-      ltid = tid_nu;                  //Definer ny tid
       //ledcWriteTone(channel, freq * !ledstate);
-      analogWrite(5, 100 * !ledstate);
-    }
-  };
+analogWrite(5, 100*ledstate);    
+board1.setPWM(0, 0, angleToPulse(180*ledstate) );
+//statssss = 1;
 
-  
+
+
+    }
+    /*
+    if (tid_nu > 2000 + ltid && tid_nu < 3000 + ltid && statssss == 1) {  //Dersom det har gått 500ms siden sist tid
+
+                Serial.println("ALARM 3");
+                myservo.write(180*ledstate);
+                statssss = 2;
+
+                
+    }
+    if (tid_nu > 3000 + ltid && statssss == 2){                
+      statssss = 0;
+      ltid = tid_nu;                  //Definer ny tid
+}
+
+
+*/
+    };
   if (tid_nu > alarmlengde + alarmtid) {
     alarmos = 0; // Skru av alarm
-    digitalWrite(ledPin, LOW);
-    analogWrite(5, 0);
+
+  };
+  if(alarmos == 0){
+        digitalWrite(ledPin, LOW);
+analogWrite(5, 0);    
     //ledcWriteTone(channel, 0);
     // myservo.write(180);
-  };
+    }
   
   
   if (tid_nu > tid + 20000) {
+
+    
     Serial.println("max");
     // PRINT DEM FØRST
     // Serial.print("maxverdiTemp: " );
@@ -364,7 +416,8 @@ void loop() {
     if (terskel_teller >= 2) {
       alarmos = 1;
       alarmtid = tid_nu;
-      Blynk.notify("ALARM");
+      Serial.println("ALARM");
+      //Blynk.notify("ALARM");
     
     }
     
